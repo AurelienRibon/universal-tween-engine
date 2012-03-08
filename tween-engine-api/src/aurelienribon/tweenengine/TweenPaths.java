d856b2605f0f62b4ca5df69438448231b92b80d0
AurelienRibon <aurelien.ribon@gmail.com>
2012-03-08 17:01:14 +0100

    + added TweenPaths, a collection of paths, + added TweenEquations, a collection of equations, + added missing javadocs, * moved existing utilities to TweenUtils

--- tween-engine-api/src/aurelienribon/tweenengine/TweenPaths.java
+++ tween-engine-api/src/aurelienribon/tweenengine/TweenPaths.java
@@ -4,9 +4,11 @@ import aurelienribon.tweenengine.paths.CatmullRom;
 import aurelienribon.tweenengine.paths.Linear;
 
 /**
+ * Collection of built-in paths.
+ *
  * @author Aurelien Ribon | http://www.aurelienribon.com/
  */
-public class TweenPaths {
+public interface TweenPaths {
 	public static final Linear linear = new Linear();
 	public static final CatmullRom catmullRom = new CatmullRom();
 }
